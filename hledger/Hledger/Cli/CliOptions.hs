bf5862096fa35397fe0ae1d9a8cd283adf51e794
Jakob Schöttl <jschoett@gmail.com>
2019-05-01 22:29:42 +0200

    Bugfix in documentation: last reporting option takes precedence

--- hledger/Hledger/Cli/CliOptions.hs
+++ hledger/Hledger/Cli/CliOptions.hs
@@ -138,7 +138,7 @@ reportflags = [
  ,flagNone ["monthly","M"]   (setboolopt "monthly") "multiperiod/multicolumn report by month"
  ,flagNone ["quarterly","Q"] (setboolopt "quarterly") "multiperiod/multicolumn report by quarter"
  ,flagNone ["yearly","Y"]    (setboolopt "yearly") "multiperiod/multicolumn report by year"
- ,flagReq  ["period","p"]    (\s opts -> Right $ setopt "period" s opts) "PERIODEXP" "set start date, end date, and/or report interval all at once (overrides the flags above)"
+ ,flagReq  ["period","p"]    (\s opts -> Right $ setopt "period" s opts) "PERIODEXP" "set start date, end date, and/or report interval all at once"
  ,flagNone ["date2"]         (setboolopt "date2") "match the secondary date instead (see command help for other effects)"
 
  ,flagNone ["unmarked","U"]  (setboolopt "unmarked") "include only unmarked postings/txns (can combine with -P or -C)"
